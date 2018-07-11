1d5c4c93abc93af1127a98b61ddd523b1804b4cb
Khuong Huu Pham Duy <phamduydtvt08@gmail.com>
2018-07-11 15:47:35 +0700

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/nguyen/luan/getcard/fragment/ChangeCardFragment.kt
+++ app/src/main/java/nguyen/luan/getcard/fragment/ChangeCardFragment.kt
@@ -1,16 +1,20 @@
 package nguyen.luan.getcard.fragment
 
  import android.app.Dialog
+ import android.os.AsyncTask
+ import android.app.ProgressDialog
  import android.os.Bundle
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+ import android.widget.Toast
  import androidx.fragment.app.Fragment
 
  import com.bumptech.glide.Glide
  import kotlinx.android.synthetic.main.fragment_user.*
  import nguyen.luan.getcard.R
+ import nguyen.luan.getcard.Utils.GooglePlayScraper
  import nguyen.luan.getcard.Utils.ScreenPreference
 
 
@@ -30,6 +34,7 @@ class ChangeCardFragment : Fragment(), View.OnClickListener {
         Glide.with(activity).load(url).error(R.drawable.ic_launcher_background).into(imageView)
         user_name.text = ScreenPreference.instance.saveName
         btnAdd.setOnClickListener {
+            ScrapePlayStoreTask().execute("biz.gina.southernbreezetour")
             val d = Dialog(activity)
             d.setContentView(R.layout.input_dialog)
             d.show()
@@ -42,7 +47,36 @@ class ChangeCardFragment : Fragment(), View.OnClickListener {
         return view
     }
 
+    internal inner class ScrapePlayStoreTask : AsyncTask<String, Void, GooglePlayScraper.AppDetails>() {
 
+        private var progressDialog: ProgressDialog? = null
+
+        override fun onPreExecute() {
+            progressDialog = ProgressDialog.show(activity, "Please Wait...", "Scraping Website...")
+        }
+
+        override fun doInBackground(vararg params: String): GooglePlayScraper.AppDetails? {
+            try {
+                return GooglePlayScraper.scrapeAppDetails(params[0])
+            } catch (e: GooglePlayScraper.WebScrapeException) {
+                e.printStackTrace()
+                return null
+            }
+
+        }
+
+        override fun onPostExecute(appDetails: GooglePlayScraper.AppDetails?) {
+            progressDialog!!.dismiss()
+
+            if (appDetails == null) {
+                Toast.makeText(activity, "Error scraping app details", Toast.LENGTH_LONG).show()
+                return
+            }
+
+
+        }
+
+    }
 
 
 }
